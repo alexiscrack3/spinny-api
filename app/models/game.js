0578a643e7f8be5ba37267df1b2c2db3374f7186
Alexis Ortega <alexis.ortega@asynchrony.com>
2019-06-22 21:53:35 -0600

    Add created_at on game schema

--- app/models/game.js
+++ app/models/game.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const gameSchema = new Schema({
+    created_at: { type: Date, default: Date.now },
     winner: { type: Schema.Types.ObjectId, ref: 'Player', required: true },
     looser: { type: Schema.Types.ObjectId, ref: 'Player', required: true },
 });
