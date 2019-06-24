6a951d52f1f924336cc12412984ad109926aad94
Alexis Ortega <alexis.oc3@gmail.com>
2019-06-24 10:12:15 -0600

    Add body parser module

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,10 @@
+const bodyParser = require('./bodyParser');
 const cors = require('./cors');
 const express = require('./express');
 const morgan = require('./morgan');
 
 module.exports = {
+    bodyParser,
     cors,
     express,
     morgan,
