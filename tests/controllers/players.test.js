15093f366de30348c5ea93f061f68fc1b64dc70a
Alexis Ortega <alexis.oc3@gmail.com>
2020-05-22 17:40:58 -0600

    Fix player tests

--- tests/controllers/players.test.js
+++ tests/controllers/players.test.js
@@ -3,180 +3,57 @@ const sinon = require('sinon');
 const PlayersController = require('../../app/controllers/players');
 const Player = require('../../app/models/player');
 
-describe('players controller', () => {
+describe('PlayersController', () => {
     beforeEach(() => {
         sinon.restore();
     });
 
     describe('create', () => {
-        it('should create player', (done) => {
+        it('should invoke create on model', () => {
             const body = {
-                email: 'user@spinny.com',
+                email: 'user@spinny.io',
             };
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('create')
-                .withArgs(body)
-                .resolves(body);
+            const stub = sinon.stub(Player, 'create');
+            const testObject = new PlayersController(Player);
 
-            const playersController = new PlayersController(Player);
-            playersController.create(body).then((player) => {
-                playerMock.verify();
-                playerMock.restore();
-                expect(body).toBe(player);
-                done();
-            }).catch((err) => {
-                done(err);
-            });
-        });
+            testObject.create(body);
 
-        it('should return error', (done) => {
-            const body = {
-                email: 'user@spinny.com',
-            };
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('create')
-                .withArgs(body)
-                .returns(Promise.reject());
-
-            const playersController = new PlayersController(Player);
-            playersController.create(body).then(() => {
-                done();
-            }).catch((err) => {
-                playerMock.verify();
-                playerMock.restore();
-                done(err);
-            });
+            expect(stub.calledWith(body)).toBeTruthy();
         });
     });
 
     describe('getById', () => {
-        it('should get player by id', (done) => {
-            const id = new mongoose.Types.ObjectId();
-            const body = {
-                _id: id,
-                email: 'user@spinny.com',
-            };
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('findById')
-                .withArgs(id.toHexString())
-                .resolves(body);
+        it('should invoke findById on model', () => {
+            const id = new mongoose.Types.ObjectId().toHexString();
+            const spy = sinon.spy(Player, 'findById');
+            const testObject = new PlayersController(Player);
 
-            const playersController = new PlayersController(Player);
-            playersController.getById(id.toHexString()).then((player) => {
-                playerMock.verify();
-                playerMock.restore();
-                expect(body).toBe(player);
-                done();
-            }).catch((err) => {
-                done(err);
-            });
-        });
+            testObject.getById(id);
 
-        it('should return error', (done) => {
-            const id = new mongoose.Types.ObjectId();
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('findById')
-                .withArgs(id.toHexString())
-                .returns(Promise.reject());
-
-            const playersController = new PlayersController(Player);
-            playersController.getById(id.toHexString()).then(() => {
-                done();
-            }).catch((err) => {
-                playerMock.verify();
-                playerMock.restore();
-                done(err);
-            });
+            expect(spy.calledWith(id)).toBeTruthy();
         });
     });
 
     describe('getByEmail', () => {
-        it('should get player by email', (done) => {
-            const body = {
-                email: 'user@spinny.com',
-            };
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('findOne')
-                .withArgs({ email: body.email })
-                .resolves(body);
+        it('should invoke findOne with email on model', () => {
+            const email = 'user@spinny.io';
+            const spy = sinon.spy(Player, 'findOne');
+            const testObject = new PlayersController(Player);
 
-            const playersController = new PlayersController(Player);
-            playersController.getByEmail(body.email).then((player) => {
-                playerMock.verify();
-                playerMock.restore();
-                expect(body).toBe(player);
-                done();
-            }).catch((err) => {
-                done(err);
-            });
-        });
+            testObject.getByEmail(email);
 
-        it('should return error', (done) => {
-            const body = {
-                email: 'user@spinny.com',
-            };
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('findOne')
-                .withArgs({ email: body.email })
-                .returns(Promise.reject());
-
-            const playersController = new PlayersController(Player);
-            playersController.getByEmail(body.email).then(() => {
-                done();
-            }).catch((err) => {
-                playerMock.verify();
-                playerMock.restore();
-                done(err);
-            });
+            expect(spy.calledWith({ email })).toBeTruthy();
         });
     });
 
     describe('getAll', () => {
-        it('should get players', (done) => {
-            const body = {
-                email: 'user@spinny.com',
-            };
-            const results = [body];
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('find')
-                .resolves(results);
+        it('should invoke find on model', () => {
+            const testObject = new PlayersController(Player);
+            const spy = sinon.spy(Player, 'find');
 
-            const playersController = new PlayersController(Player);
-            playersController.getAll().then((players) => {
-                playerMock.verify();
-                playerMock.restore();
-                expect(results).toBe(players);
-                done();
-            }).catch((err) => {
-                done(err);
-            });
-        });
+            testObject.getAll();
 
-        it('should return error', (done) => {
-            const body = {
-                email: 'user@spinny.com',
-            };
-            const results = [body];
-            const playerMock = sinon.mock(Player);
-            playerMock
-                .expects('find')
-                .resolves(results);
-
-            const playersController = new PlayersController(Player);
-            playersController.getAll().then(() => {
-                done();
-            }).catch((err) => {
-                playerMock.verify();
-                playerMock.restore();
-                done(err);
-            });
+            expect(spy.calledOnce).toBeTruthy();
         });
     });
 });
