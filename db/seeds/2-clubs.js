ded7d053c214caf7cfddb061d966f7f2bf8e7fc1
Alexis Ortega <alexis.oc3@gmail.com>
2020-05-22 19:56:54 -0600

    Add new seed file for clubs

---
+++ db/seeds/2-clubs.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const faker = require('faker');
+const Club = require('../../app/models/club');
+
+const clubs = [];
+for (let index = 0; index < 10; index += 1) {
+    const club = new Club({
+        name: faker.random.words().toLowerCase(),
+    });
+    clubs.push(club);
+}
+
+module.exports = clubs;
