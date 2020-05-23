07c02441f5ab973cbbddaa083df9c9529417e9b5
Alexis Ortega <alexis.oc3@gmail.com>
2020-05-22 18:08:11 -0600

    Fix game tests

--- tests/controllers/games.test.js
+++ tests/controllers/games.test.js
@@ -1,71 +1,11 @@
 const mongoose = require('mongoose');
 const sinon = require('sinon');
-
 const GamesController = require('../../app/controllers/games');
 const Game = require('../../app/models/game');
 
-describe('games controller', () => {
+describe('GamesController', () => {
     beforeEach(() => {
         sinon.restore();
-    });
-
-    describe('deleteById', () => {
-        it('should delete game', (done) => {
-            const id = new mongoose.Types.ObjectId();
-            const body = {
-                _id: id,
-                winner: new mongoose.Types.ObjectId(),
-                loser: new mongoose.Types.ObjectId(),
-            };
-            const gameMock = sinon.mock(Game);
-            gameMock
-                .expects('findOneAndRemove')
-                .withArgs({ _id: id.toHexString() })
-                .yields(null, body);
-
-            const gamesController = new GamesController(Game);
-            gamesController.deleteById(id.toHexString()).then((game) => {
-                gameMock.verify();
-                gameMock.restore();
-                expect(body).toBe(game);
-                done();
-            }).catch((err) => {
-                done(err);
-            });
-        });
-
-        it('should return error', () => {
-            const id = new mongoose.Types.ObjectId();
-            const error = new Error();
-            const gameMock = sinon.mock(Game);
-            gameMock
-                .expects('findOneAndRemove')
-                .withArgs({ _id: id.toHexString() })
-                .yields(error, null);
-
-            const gamesController = new GamesController(Game);
-            return gamesController.deleteById(id.toHexString()).catch((err) => {
-                gameMock.verify();
-                gameMock.restore();
-                expect(err).toBe(error);
-            });
-        });
-
-        it('should return error when id does not exist', () => {
-            const id = new mongoose.Types.ObjectId();
-            const gameMock = sinon.mock(Game);
-            gameMock
-                .expects('findOneAndRemove')
-                .withArgs({ _id: id.toHexString() })
-                .yields(null, null);
-
-            const gamesController = new GamesController(Game);
-            return gamesController.deleteById(id.toHexString()).catch((err) => {
-                gameMock.verify();
-                gameMock.restore();
-                expect(err).toEqual(new Error('Game id does not exist'));
-            });
-        });
     });
 
     describe('create', () => {
@@ -74,15 +14,15 @@ describe('games controller', () => {
                 winner: new mongoose.Types.ObjectId(),
                 loser: new mongoose.Types.ObjectId(),
             };
-            const gameMock = sinon.mock(Game);
-            gameMock
+            const mock = sinon.mock(Game);
+            mock
                 .expects('create')
                 .resolves(body);
 
-            const gamesController = new GamesController(Game);
-            gamesController.create(body).then((game) => {
-                gameMock.verify();
-                gameMock.restore();
+            const testObject = new GamesController(Game);
+            testObject.create(body).then((game) => {
+                mock.verify();
+                mock.restore();
                 expect(body).toBe(game);
                 done();
             }).catch((err) => {
@@ -97,9 +37,68 @@ describe('games controller', () => {
                 loser: id,
             };
 
-            const gamesController = new GamesController(Game);
-            return gamesController.create(body).catch((err) => {
+            const testObject = new GamesController(Game);
+            return testObject.create(body).catch((err) => {
                 expect(err).toEqual(new Error('Winner and loser cannot be same player'));
+            });
+        });
+    });
+
+    describe('deleteById', () => {
+        it('should delete game', (done) => {
+            const id = new mongoose.Types.ObjectId();
+            const body = {
+                _id: id,
+                winner: new mongoose.Types.ObjectId(),
+                loser: new mongoose.Types.ObjectId(),
+            };
+            const mock = sinon.mock(Game);
+            mock
+                .expects('findOneAndRemove')
+                .withArgs({ _id: id.toHexString() })
+                .yields(null, body);
+
+            const testObject = new GamesController(Game);
+            testObject.deleteById(id.toHexString()).then((game) => {
+                mock.verify();
+                mock.restore();
+                expect(body).toBe(game);
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+
+        it('should return error', () => {
+            const id = new mongoose.Types.ObjectId();
+            const error = new Error();
+            const mock = sinon.mock(Game);
+            mock
+                .expects('findOneAndRemove')
+                .withArgs({ _id: id.toHexString() })
+                .yields(error, null);
+
+            const testObject = new GamesController(Game);
+            return testObject.deleteById(id.toHexString()).catch((err) => {
+                mock.verify();
+                mock.restore();
+                expect(err).toBe(error);
+            });
+        });
+
+        it('should return error when id does not exist', () => {
+            const id = new mongoose.Types.ObjectId();
+            const mock = sinon.mock(Game);
+            mock
+                .expects('findOneAndRemove')
+                .withArgs({ _id: id.toHexString() })
+                .yields(null, null);
+
+            const testObject = new GamesController(Game);
+            return testObject.deleteById(id.toHexString()).catch((err) => {
+                mock.verify();
+                mock.restore();
+                expect(err).toEqual(new Error('Game id does not exist'));
             });
         });
     });
@@ -118,8 +117,8 @@ describe('games controller', () => {
                 exec: sinon.stub().resolves(results),
             });
 
-            const gamesController = new GamesController(Game);
-            gamesController.getAll().then((games) => {
+            const testObject = new GamesController(Game);
+            testObject.getAll().then((games) => {
                 gameStub.restore();
                 expect(results).toBe(games);
                 done();
