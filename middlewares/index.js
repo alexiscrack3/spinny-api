0539df7e7891c4dfb34a2d26403433ba1d10bc44
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-29 12:53:04 -0600

    Use actuator dependency

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
+const actuator = require('./actuator');
 const bodyParser = require('./body-parser');
 const cors = require('./cors');
 const express = require('./express');
 const morgan = require('./morgan');
 
 module.exports = {
+    actuator,
     bodyParser,
     cors,
     express,
