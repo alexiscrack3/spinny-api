281a24532e03525841202a83abf9fd895e0c6be0
Alexis Ortega <alexis.oc3@gmail.com>
2020-10-27 14:59:14 -0600

    Return default json response for any error

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const createError = require('http-errors');
 const debug = require('debug')('spinny:app');
 const middlewares = require('./middlewares');
 const routes = require('./routes');
@@ -25,18 +24,15 @@ if (process.env.NODE_ENV !== 'test') {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    next(createError(404));
-});
-
-// error handler
-app.use((err, req, res) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    // res.render('error', { title: 'error' });
+    const body = {
+        timestamp: new Date().toISOString(),
+        status: 404,
+        error: 'Not Found',
+        message: '',
+        path: req.path,
+    };
+    res.status(404).json(body);
+    next();
 });
 
 function gracefulExit() {
