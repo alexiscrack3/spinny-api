fe15f2688732d0442190db417b3061c84edb0740
Alexis Ortega <alexis.oc3@gmail.com>
2022-04-25 15:15:28 -0600

    Annotate players model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: players
+#
+#  id         :bigint           not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Player < ApplicationRecord
   validates_presence_of :first_name, message: "can't be blank"
   validates :last_name, presence: true
