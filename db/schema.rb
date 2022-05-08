a6b029cca9af2fdcf53e3cc9f8373dfb87b1b030
Alexis Ortega <alexis.oc3@gmail.com>
2022-05-08 15:50:20 -0600

    Add scaffold for clubs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_08_213933) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_08_214636) do
+  create_table "clubs", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "players", charset: "utf8mb4", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
