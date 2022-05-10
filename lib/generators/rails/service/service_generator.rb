ff1ee709fb278611b0100c2f97940ca314e2b7af
Alexis Ortega <alexis.oc3@gmail.com>
2022-05-09 23:49:04 -0600

    Update service generator to create test file

--- lib/generators/rails/service/service_generator.rb
+++ lib/generators/rails/service/service_generator.rb
@@ -1,28 +1,59 @@
 class Rails::ServiceGenerator < Rails::Generators::NamedBase
-  def create_service_file
-    create_file "app/services/#{plural_name}_service.rb", <<-FILE
-class #{plural_name.capitalize}Service
-  def #{plural_name}
-    #{plural_name.singularize.capitalize}.all
+  def create_files
+    create_service_file
+    create_test_file
   end
 
-  def #{plural_name.singularize}(id)
-    #{plural_name.singularize.capitalize}.find_by(id: id)
+  private
+
+  def plural_model_name
+    plural_name.capitalize
+  end
+
+  def singular_model_name
+    plural_name.singularize
+  end
+
+  def model_name
+    singular_name.capitalize
+  end
+
+  def create_service_file
+    create_file "app/services/#{plural_name}_service.rb", <<-FILE
+class #{plural_model_name}Service
+  def #{plural_name}
+    #{model_name}.all
+  end
+
+  def #{singular_model_name}(id)
+    #{model_name}.find_by(id: id)
   end
 
   def create(params)
-    #{plural_name.singularize} = #{plural_name.singularize.capitalize}.new(params)
-    #{plural_name.singularize}.save
+    #{singular_model_name} = #{model_name}.new(params)
+    #{singular_model_name}.save
   end
 
   def update(id, params)
-    #{plural_name.singularize} = #{plural_name.singularize.capitalize}.find_by(id: id)
-    #{plural_name.singularize}.update(params)
+    #{singular_model_name} = #{model_name}.find_by(id: id)
+    #{singular_model_name}.update(params)
   end
 
   def delete(id)
-    #{plural_name.singularize.capitalize}.destroy(id)
+    #{model_name}.destroy(id)
   end
+end
+    FILE
+  end
+
+  def create_test_file
+    create_file "test/services/#{plural_name}_service_test.rb", <<-FILE
+require 'test_helper'
+
+class #{plural_model_name}ServiceTest < ActiveSupport::TestCase
+  # test 'the truth' do
+  #   assert true
+  # end
 end
     FILE
   end
