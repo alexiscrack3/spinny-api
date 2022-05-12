0fd720955f1f22b2e2852490c34c4f0fc957637d
Alexis Ortega <alexis.oc3@gmail.com>
2022-05-12 10:59:27 -0600

    Fix errors in service generator with rubocop

--- lib/generators/rails/service/service_generator.rb
+++ lib/generators/rails/service/service_generator.rb
@@ -1,60 +1,64 @@
-class Rails::ServiceGenerator < Rails::Generators::NamedBase
-  def create_files
-    create_service_file
-    create_test_file
-  end
+# frozen_string_literal: true
 
-  private
+module Rails
+  class ServiceGenerator < Rails::Generators::NamedBase
+    def create_files
+      create_service_file
+      create_test_file
+    end
 
-  def plural_model_name
-    plural_name.capitalize
-  end
+    private
 
-  def singular_model_name
-    plural_name.singularize
-  end
+    def plural_model_name
+      plural_name.capitalize
+    end
 
-  def model_name
-    singular_name.capitalize
-  end
+    def singular_model_name
+      plural_name.singularize
+    end
 
-  def create_service_file
-    create_file "app/services/#{plural_name}_service.rb", <<-FILE
-class #{plural_model_name}Service
-  def #{plural_name}
-    #{model_name}.all
-  end
+    def model_name
+      singular_name.capitalize
+    end
 
-  def #{singular_model_name}(id)
-    #{model_name}.find_by(id: id)
-  end
+    def create_service_file
+      create_file "app/services/#{plural_name}_service.rb", <<~FILE
+        class #{plural_model_name}Service
+          def #{plural_name}
+            #{model_name}.all
+          end
 
-  def create(params)
-    #{singular_model_name} = #{model_name}.new(params)
-    #{singular_model_name}.save
-  end
+          def #{singular_model_name}(id)
+            #{model_name}.find_by(id: id)
+          end
 
-  def update(id, params)
-    #{singular_model_name} = #{model_name}.find_by(id: id)
-    #{singular_model_name}.update(params)
-  end
+          def create(params)
+            #{singular_model_name} = #{model_name}.new(params)
+            #{singular_model_name}.save
+          end
 
-  def delete(id)
-    #{model_name}.destroy(id)
-  end
-end
-    FILE
-  end
+          def update(id, params)
+            #{singular_model_name} = #{model_name}.find_by(id: id)
+            #{singular_model_name}.update(params)
+          end
 
-  def create_test_file
-    create_file "test/services/#{plural_name}_service_test.rb", <<-FILE
-require 'test_helper'
+          def delete(id)
+            #{model_name}.destroy(id)
+          end
+        end
+      FILE
+    end
 
-class #{plural_model_name}ServiceTest < ActiveSupport::TestCase
-  # test 'the truth' do
-  #   assert true
-  # end
-end
-    FILE
+    def create_test_file
+      create_file "test/services/#{plural_name}_service_test.rb", <<~FILE
+        require "test_helper"
+
+        class #{plural_model_name}ServiceTest < ActiveSupport::TestCase
+          # test "the truth" do
+          #   assert true
+          # end
+        end
+      FILE
+    end
   end
 end
--- test/lib/generators/rails/service_generator_test.rb
+++ test/lib/generators/rails/service_generator_test.rb
@@ -3,14 +3,16 @@
 require "test_helper"
 require "generators/rails/service/service_generator"
 
-class Rails::ServiceGeneratorTest < Rails::Generators::TestCase
-  tests Rails::ServiceGenerator
-  destination Rails.root.join("tmp/generators")
-  setup :prepare_destination
+module Rails
+  class ServiceGeneratorTest < Rails::Generators::TestCase
+    tests Rails::ServiceGenerator
+    destination Rails.root.join("tmp/generators")
+    setup :prepare_destination
 
-  # test "generator runs without errors" do
-  #   assert_nothing_raised do
-  #     run_generator ["arguments"]
-  #   end
-  # end
+    # test "generator runs without errors" do
+    #   assert_nothing_raised do
+    #     run_generator ["arguments"]
+    #   end
+    # end
+  end
 end
