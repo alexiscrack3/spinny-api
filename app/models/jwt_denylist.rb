0f0e0efd929ed71f2681f0b8ecb3c6f8b98a85e6
Alexis Ortega <alexis.oc3@gmail.com>
2022-12-27 00:16:16 -0800

    Annotate jwt_denylist model

--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: jwt_denylist
+#
+#  id  :bigint           not null, primary key
+#  jti :string(255)      not null
+#  exp :datetime         not null
+#
 class JwtDenylist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Denylist
 
