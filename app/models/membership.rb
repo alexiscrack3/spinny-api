ec9a0649f84c89a8c56a842fa57371b37eb6c3a1
Alexis Ortega <alexis.oc3@gmail.com>
2022-12-26 19:02:59 -0800

    Annotate membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id         :bigint           not null, primary key
+#  player_id  :bigint           not null
+#  club_id    :bigint           not null
+#  rating     :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Membership < ApplicationRecord
   belongs_to :player
   belongs_to :club
