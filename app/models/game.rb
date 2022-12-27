6570d0022981a1da69d4243ee56be73108f8e02c
Alexis Ortega <alexis.oc3@gmail.com>
2022-12-27 00:16:36 -0800

    Annotate game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :bigint           not null, primary key
+#  winner_id  :bigint           not null
+#  loser_id   :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Game < ApplicationRecord
   belongs_to :winner, class_name: "Membership"
   belongs_to :loser, class_name: "Membership"
