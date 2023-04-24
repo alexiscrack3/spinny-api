f73001d367a6bfabfda97537487a8dba24fb1ab6
Alexis Ortega <alexis.oc3@gmail.com>
2023-04-23 23:11:39 -0600

    Remove unncessary parameters

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 class ClubsController < ApplicationController
-  before_action :authenticate_player!, only: [:index, :create, :show, :update, :destroy, :members, :join, :leave]
+  before_action :authenticate_player!
 
   sig { void }
   def initialize
