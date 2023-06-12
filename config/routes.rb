81e8d4509c0e855df8e58debdf961b49dd86caf5
Alexis Ortega <alexis.oc3@gmail.com>
2023-06-11 18:08:01 -0600

    Add API scope for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,42 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :players, path: 'players', path_names: {
-    sign_in: 'sign_in',
-    sign_out: 'sign_out',
-    registration: 'sign_up'
-  },
-  controllers: {
-      sessions: 'sessions',
-      registrations: 'registrations'
-  }
-  namespace :admin do
-    get 'clubs', to: 'clubs#index'
-  end
+  scope '/api' do
+    devise_for :players, path: 'players', path_names: {
+      sign_in: 'sign_in',
+      sign_out: 'sign_out',
+      registration: 'sign_up'
+    },
+    controllers: {
+        sessions: 'sessions',
+        registrations: 'registrations'
+    }
+    namespace :admin do
+      get 'clubs', to: 'clubs#index'
+    end
 
-  get 'players/me', to: 'players#me'
-  resources :players, only: %i[index show update]
+    get 'players/me', to: 'players#me'
+    resources :players, only: %i[index show update]
 
-  resources :clubs do
-    get :members, to: 'clubs#members'
-    post :members, to: 'clubs#join'
-    delete :members, to: 'clubs#leave'
-  end
-  resources :games, except: %i[index]
+    resources :clubs do
+      get :members, to: 'clubs#members'
+      post :members, to: 'clubs#join'
+      delete :members, to: 'clubs#leave'
+    end
+    resources :games, except: %i[index]
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    # root "articles#index"
 
-  post '/graphql', to: 'graphql#execute'
-  if Rails.env.development?
-    mount Sidekiq::Web => '/sidekiq'
+    post '/graphql', to: 'graphql#execute'
+    if Rails.env.development?
+      mount Sidekiq::Web => '/sidekiq'
 
-    mount GraphiQL::Rails::Engine,
-          at: '/graphiql',
-          graphql_path: 'graphql#execute'
+      mount GraphiQL::Rails::Engine,
+            at: '/graphiql',
+            graphql_path: 'graphql#execute'
+    end
   end
 end
